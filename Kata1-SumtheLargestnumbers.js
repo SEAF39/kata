d908055d126c76a01efabc7e1d69f0aa208329f5
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-21 17:29:55 +0000

    Kata 1 - Sum the Largest Numbers

--- Kata1-SumtheLargestnumbers.js
+++ Kata1-SumtheLargestnumbers.js
@@ -1,6 +1,7 @@
-
 const sumLargestNumbers = function(data) {
   // Put your solution here
+  // with if statement
+  /*
   let number1 = data[0];
   let number2 = data[0];
   for(let number of data){
@@ -12,6 +13,17 @@ const sumLargestNumbers = function(data) {
     }
   }
   return number1+number2;
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+*/
+
+// with buitl-in funcation .sort
+{
+  let sortNumber = data.sort(function (a, b) { return b - a });
+  return sortNumber[0] + sortNumber[1];
 };
 
 console.log(sumLargestNumbers([1, 10]));
