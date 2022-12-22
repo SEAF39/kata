37e7aab368eacc988154241c64a4299c24896e24
SEAF39 <seaf.roshi.37@gmail.com>
2022-12-22 21:38:54 +0000

    Kata7-IntheAirTonight

---
+++ Kata7-IntheAirTonight.js
@@ -0,0 +1,34 @@
+
+
+  const checkAir = function (samples, threshold)
+    // Code here! 
+  {
+    let dirtyCount = 0;
+    for (const sample of samples) {
+      if (sample === 'dirty') {
+        dirtyCount += 1;
+      }
+    }
+    const proportionDirty = dirtyCount / samples.length;
+    if (proportionDirty >= threshold) {
+      return 'Polluted';
+    } else {
+      return 'Clean';
+    }
+  };
+  
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
+
